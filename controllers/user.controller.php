8e58d50f97d6be0545993bb2c6a37f8918b8b3fe
MarioMiles <77443888+MarioMiles@users.noreply.github.com>
2022-06-09 17:50:10 +0200

    Update user.controller.php

--- controllers/user.controller.php
+++ controllers/user.controller.php
@@ -1,6 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json, charset=utf-8');
 use \Firebase\JWT\JWT;
 
 class UserController {
