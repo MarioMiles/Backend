dd484d14b6716aa6f2b87a2591f33e7b9a539ed4
MarioMiles <77443888+MarioMiles@users.noreply.github.com>
2022-06-09 17:52:50 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,4 +2,6 @@
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Europe/Madrid');
 header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE");
-header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json, charset=utf-8');
